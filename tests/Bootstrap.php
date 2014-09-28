bea9f4f7408c01da0dc380111df82121533e76c3
mamuz <muzzi_is@web.de>
2014-09-28 20:47:38 +0200

    add bootstrap for UT

---
+++ tests/Bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(E_ALL);
+
+$file = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($file)) {
+    $loader = require $file;
+}
+
+if (!isset($loader)) {
+    throw new \RuntimeException('Cannot find vendor/autoload.php');
+}
+
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader->add('PhpDaTest\\', __DIR__);
+
+unset($file, $loader);
