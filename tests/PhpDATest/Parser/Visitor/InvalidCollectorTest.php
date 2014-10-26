03c4ba5a74424229a861f1bc8df25d2cabd14662
mamuz <muzzi_is@web.de>
2014-10-26 12:52:03 +0100

    add UTs

---
+++ tests/PhpDATest/Parser/Visitor/InvalidCollectorTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Marco Muths
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace PhpDATest\Parser\Visitor;
+
+use PhpDATest\Parser\Visitor\Stub\InvalidCollector;
+
+class InvalidCollectorTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var InvalidCollector */
+    protected $fixture;
+
+    /** @var \PhpDA\Parser\Filter\NodeNameInterface | \Mockery\MockInterface */
+    protected $nodeNameFilter;
+
+    protected function setUp()
+    {
+        $this->adt = \Mockery::mock('PhpDA\Entity\Adt');
+        $this->nodeNameFilter = \Mockery::mock('PhpDA\Parser\Filter\NodeNameInterface');
+
+        $this->fixture = new InvalidCollector;
+        $this->fixture->setNodeNameFilter($this->nodeNameFilter);
+    }
+
+    public function testCollecting()
+    {
+        $this->setExpectedException('RuntimeException');
+        $node = \Mockery::mock('PhpParser\Node\Name');
+        $this->nodeNameFilter->shouldReceive('filter')->once()->andReturnUsing(
+            function ($object) {
+                return $object;
+            }
+        );
+        $this->fixture->leaveNode($node);
+    }
+}
