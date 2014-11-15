fb2440d9bfb28cf2fb9be0f9b89247b6f235f9be
mamuz <muzzi_is@web.de>
2014-11-15 13:35:01 +0100

    add UTs

---
+++ tests/PhpDATest/Parser/Visitor/NamespacedStringCollectorTest.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Marco Muths
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace PhpDATest\Parser\Visitor;
+
+use PhpDA\Parser\Visitor\NamespacedStringCollector;
+
+class NamespacedStringCollectorTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var NamespacedStringCollector */
+    protected $fixture;
+
+    /** @var \PhpDA\Entity\Adt | \Mockery\MockInterface */
+    protected $adt;
+
+    /** @var \PhpDA\Parser\Filter\NodeNameInterface | \Mockery\MockInterface */
+    protected $nodeNameFilter;
+
+    protected function setUp()
+    {
+        $this->adt = \Mockery::mock('PhpDA\Entity\Adt');
+        $this->nodeNameFilter = \Mockery::mock('PhpDA\Parser\Filter\NodeNameInterface');
+
+        $this->fixture = new NamespacedStringCollector;
+        $this->fixture->setAdt($this->adt);
+        $this->fixture->setNodeNameFilter($this->nodeNameFilter);
+    }
+
+    public function testNotCollectingByInvalidNode()
+    {
+        $node = \Mockery::mock('PhpParser\Node');
+        $this->fixture->leaveNode($node);
+    }
+
+    public function testCollectingByFilteredToNull()
+    {
+        $node = \Mockery::mock('PhpParser\Node\Scalar\String');
+        $this->nodeNameFilter->shouldReceive('filter')->andReturn(null);
+        $this->fixture->leaveNode($node);
+    }
+
+    public function testNotCollectingByClassNameWithoutParentNamespace()
+    {
+        $node = \Mockery::mock('PhpParser\Node\Scalar\String');
+        $node->value = 'Foo';
+        $this->fixture->leaveNode($node);
+    }
+
+    public function testNotCollectingByInvalidClassName()
+    {
+        $node = \Mockery::mock('PhpParser\Node\Scalar\String');
+        $node->value = '?Foo';
+        $this->fixture->leaveNode($node);
+    }
+
+    public function testNotCollectingByInvalidNamespaces()
+    {
+        $node = \Mockery::mock('PhpParser\Node\Scalar\String');
+        $node->value = 'Foo\?Bar';
+        $this->fixture->leaveNode($node);
+    }
+
+    public function testNotCollectingByInvalidNamespaceSeparator()
+    {
+        $node = \Mockery::mock('PhpParser\Node\Scalar\String');
+        $node->value = 'Foo_Bar';
+        $this->fixture->leaveNode($node);
+    }
+
+    public function testCollecting()
+    {
+        $testcase = $this;
+        $attributes = array('foo' => 'bar');
+        $node = \Mockery::mock('PhpParser\Node\Scalar\String');
+        $node->shouldReceive('getAttributes')->andReturn($attributes);
+        $node->value = 'Foo\Bar';
+        $this->nodeNameFilter->shouldReceive('filter')->once()->andReturnUsing(
+            function ($object) {
+                return $object;
+            }
+        );
+        $this->adt->shouldReceive('addNamespacedString')->once()->andReturnUsing(
+            function ($object) use ($testcase, $attributes) {
+                /** @var \PhpParser\Node\Name $object */
+                $testcase->assertInstanceOf('PhpParser\Node\Name', $object);
+                $testcase->assertSame($object->toString(), '?\\Foo\\Bar');
+                $testcase->assertSame($object->getAttributes(), $attributes);
+            }
+        );
+
+        $this->fixture->leaveNode($node);
+    }
+
+    public function testCollectingSmallNamespacedString()
+    {
+        $attributes = array('foo' => 'bar');
+        $node = \Mockery::mock('PhpParser\Node\Scalar\String');
+        $node->shouldReceive('getAttributes')->andReturn($attributes);
+        $node->value = '\Foo';
+        $this->nodeNameFilter->shouldReceive('filter')->once()->andReturnUsing(
+            function ($object) {
+                return $object;
+            }
+        );
+        $this->adt->shouldReceive('addNamespacedString')->once();
+
+        $this->fixture->leaveNode($node);
+    }
+
+    public function testCollectingLongNamespacedString()
+    {
+        $attributes = array('foo' => 'bar');
+        $node = \Mockery::mock('PhpParser\Node\Scalar\String');
+        $node->shouldReceive('getAttributes')->andReturn($attributes);
+        $node->value = 'Foo\Bar\Baz\Taz';
+        $this->nodeNameFilter->shouldReceive('filter')->once()->andReturnUsing(
+            function ($object) {
+                return $object;
+            }
+        );
+        $this->adt->shouldReceive('addNamespacedString')->once();
+
+        $this->fixture->leaveNode($node);
+    }
+
+    public function testCollectingNonStandrdNamespacedString()
+    {
+        $attributes = array('foo' => 'bar');
+        $node = \Mockery::mock('PhpParser\Node\Scalar\String');
+        $node->shouldReceive('getAttributes')->andReturn($attributes);
+        $node->value = '\_\bar34\_Baz\Taz';
+        $this->nodeNameFilter->shouldReceive('filter')->once()->andReturnUsing(
+            function ($object) {
+                return $object;
+            }
+        );
+        $this->adt->shouldReceive('addNamespacedString')->once();
+
+        $this->fixture->leaveNode($node);
+    }
+}
