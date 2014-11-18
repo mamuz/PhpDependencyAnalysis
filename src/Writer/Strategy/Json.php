9d4fa29c7a0323015b114a97b31efd39aacbd1ac
mamuz <muzzi_is@web.de>
2014-11-18 20:54:52 +0100

    cs-fixes

--- src/Writer/Strategy/Json.php
+++ src/Writer/Strategy/Json.php
@@ -1,27 +1,27 @@
 <?php
 /**
-* The MIT License (MIT)
-*
-* Copyright (c) 2014 Marco Muths
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Marco Muths
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace PhpDA\Writer\Strategy;
 
@@ -29,9 +29,10 @@ use Fhaculty\Graph\Graph;
 use PhpDA\Entity\AnalysisCollection;
 
 /**
- * Class JsonStrategy
- * Gives you a json encoded file. Every id of a vertex in the graph is a root identifier in the json file. Every first
- * level vertex (it has an edge TO the vertex) is in the collection to that identifier. E.g.:
+ * Gives you a json encoded file.
+ * Every id of a vertex in the graph is a root identifier in the json file.
+ * Every first level vertex (it has an edge TO the vertex) is in the collection to that identifier.
+ * E.g.:
  *
  * {
  *   "firstThing" :
@@ -40,10 +41,9 @@ use PhpDA\Entity\AnalysisCollection;
  *          "dependencyOfFirstThing2"
  *      ]
  * }
- *
- * @package PhpDA\Writer\Strategy
  */
-class Json implements StrategyInterface {
+class Json implements StrategyInterface
+{
 
     /**
      * @param AnalysisCollection $analysisCollection
@@ -52,18 +52,26 @@ class Json implements StrategyInterface {
     public function filter(AnalysisCollection $analysisCollection)
     {
         $graph = $analysisCollection->getGraph();
+
         return json_encode($this->getVertexToVerticesArray($graph));
     }
 
-    protected function getVertexToVerticesArray(Graph $graph) {
+    /**
+     * @param Graph $graph
+     * @return array
+     */
+    protected function getVertexToVerticesArray(Graph $graph)
+    {
         $vertexToVertices = array();
 
         $vertices = $graph->getVertices();
-        foreach($vertices as $from){
+        foreach ($vertices as $from) {
+            /** @var \Fhaculty\Graph\Vertex $from */
             $toVertices = $from->getVerticesEdgeTo();
-                foreach($toVertices->getVerticesDistinct() as $to) {
-                    $this->addArrayEdge($vertexToVertices, $from->getId(), $to->getId());
-                }
+            foreach ($toVertices->getVerticesDistinct() as $to) {
+                /** @var \Fhaculty\Graph\Vertex $to */
+                $this->addArrayEdge($vertexToVertices, $from->getId(), $to->getId());
+            }
         }
 
         return $vertexToVertices;
@@ -71,11 +79,12 @@ class Json implements StrategyInterface {
 
     /**
      * @param $array array
-     * @param $from string
-     * @param $to string
+     * @param $from  string
+     * @param $to    string
      */
-    protected function addArrayEdge(&$array, $from, $to) {
-        if(array_key_exists($from, $array)) {
+    protected function addArrayEdge(&$array, $from, $to)
+    {
+        if (array_key_exists($from, $array)) {
             $array[$from][] = $to;
         } else {
             $array[$from] = array($to);
