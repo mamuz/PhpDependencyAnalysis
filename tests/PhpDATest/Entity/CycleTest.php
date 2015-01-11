e8c385cb2286cc3099c77c8a4013bb2700d681a9
mamuz <muzzi_is@web.de>
2015-01-11 19:37:59 +0100

    Add cycle and edge entity

---
+++ tests/PhpDATest/Entity/CycleTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Marco Muths
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace PhpDATest\Entity;
+
+use PhpDA\Entity\Cycle;
+
+class CycleTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Cycle */
+    protected $fixture;
+
+    /** @var array */
+    protected $path = array('A', 'B', 'C', 'A');
+
+    protected function setUp()
+    {
+        $this->fixture = new Cycle($this->path);
+    }
+
+    public function testExtendingArrayCollection()
+    {
+        $this->assertInstanceOf('Doctrine\Common\Collections\ArrayCollection', $this->fixture);
+    }
+
+    public function testStringRepresentation()
+    {
+        $this->assertSame(implode(' -> ', $this->path), $this->fixture->toString());
+    }
+
+    public function testAccessEdges()
+    {
+        $expected = array(
+            'A -> B',
+            'B -> C',
+            'C -> A',
+        );
+
+        $edges = $this->fixture->getEdges();
+        foreach ($edges as $index => $edge) {
+            $this->assertSame($expected[$index], $edge->toString());
+        }
+    }
+}
