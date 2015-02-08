9bf75a8093815cffe99af7e240aa57b8a6507e20
mamuz <muzzi_is@web.de>
2015-02-08 15:09:57 +0100

    change version to dev-master

--- src/Command/MessageInterface.php
+++ src/Command/MessageInterface.php
@@ -27,7 +27,7 @@ namespace PhpDA\Command;
 
 interface MessageInterface
 {
-    const VERSION = 'v0.4.0';
+    const VERSION = 'dev-master';
 
     const NAME = 'PhpDependencyAnalyse by Marco Muths';
 
