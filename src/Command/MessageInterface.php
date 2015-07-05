9a529282774a8ab2c717c6c8e6b5e35de9b9a1af
mamuz <mamuz.de@gmail.com>
2015-07-05 12:41:20 +0200

    prepare release

--- src/Command/MessageInterface.php
+++ src/Command/MessageInterface.php
@@ -27,7 +27,7 @@ namespace PhpDA\Command;
 
 interface MessageInterface
 {
-    const VERSION = 'v0.5.3';
+    const VERSION = 'dev-master';
 
     const NAME = 'PhpDependencyAnalysis by Marco Muths';
 
