606566c5d640dd2e1e7279847c680f66969b4923
mamuz <mamuz.de@gmail.com>
2017-03-27 03:33:56 +0200

    fix update command

--- build/phar.sh
+++ build/phar.sh
@@ -4,5 +4,6 @@ composer install --no-dev
 ~/.composer/vendor/bin/box build -vv
 mv ./download/phpda.phar ./download/phpda
 mv ./download/phpda.phar.pubkey ./download/phpda.pubkey
+sha1sum ./download/phpda > ./download/phpda.version
 chmod +x ./download/phpda
 composer install
